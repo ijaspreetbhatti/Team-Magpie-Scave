e63b41af48c6a713a4c3ae370a8956cca77ee501
rudderband <25525791+rudderband@users.noreply.github.com>
2021-10-12 11:58:47 -0700

    Webpack Configuration

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const { SourceMapDevToolPlugin } = require("webpack");
+
+module.exports = {
+    entry: './src/index.js',
+    mode: 'development',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: "main.js",
+        sourceMapFilename: "main.js.map"
+    },
+    devtool: "source-map",
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.js$/,
+                enforce: "pre",
+                use: ["source-map-loader"],
+            },
+        ]
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'public'),
+        },
+        port: 9000,
+    },
+    plugins: [
+        new SourceMapDevToolPlugin({
+            filename: "[file].map"
+        }),
+    ]
+};
