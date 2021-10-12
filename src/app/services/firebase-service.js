941afe9a950d7e664cce690638415ac660643118
rudderband <25525791+rudderband@users.noreply.github.com>
2021-10-12 11:59:08 -0700

    Firebase Configuration

---
+++ src/app/services/firebase-service.js
@@ -0,0 +1,18 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from "firebase/firestore/lite";
+
+// Set the configuration for your app
+// TODO: Replace with your project's config object
+const firebaseConfig = {
+    apiKey: "AIzaSyDxn3g5UJHMRp4XBNR1JSpQU2TmRWrNek4",
+    authDomain: "test-project-for-firebas-67d41.firebaseapp.com",
+    projectId: "test-project-for-firebas-67d41",
+    storageBucket: "test-project-for-firebas-67d41.appspot.com",
+    messagingSenderId: "334134787184",
+    appId: "1:334134787184:web:b24b7c458c484f686ac8b5"
+};
+
+const app = initializeApp(firebaseConfig);
+
+// Get a reference to the database service
+export const database = getFirestore(app);
--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 import './scss/main.scss';
+import { database } from "./app/services/firebase-service";
+import { collection, getDocs } from 'firebase/firestore/lite';
+
+// Get a list of names from your database
+async function getNames(db) {
+    const usersCol = collection(db, 'users');
+    const namesSnapshot = await getDocs(usersCol);
+    const namesList = namesSnapshot.docs.map(doc => doc.data());
+    return namesList;
+}
+
+getNames(database).then(printNames);
+
+function printNames(namesList) {
+    console.log(namesList)
+}
