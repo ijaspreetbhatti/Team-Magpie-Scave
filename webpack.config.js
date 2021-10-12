1979d12b18a1d00762440126ddc956077092edfd
rudderband <25525791+rudderband@users.noreply.github.com>
2021-10-12 12:09:06 -0700

    bug_1_fix: Repo Folder Restructure

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
     },
     devServer: {
         static: {
-            directory: path.join(__dirname, 'public'),
+            directory: path.join(__dirname, '.'),
         },
         port: 9000,
     },
