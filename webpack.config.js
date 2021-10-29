39a9bc0a5e7257d26e33bd863f8cfcbafb8cf71d
Yebincho03 <lucycho0314@gmail.com>
2021-10-29 09:03:29 -0700

    menu_yourlisting

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,44 @@
-const path = require('path');
+const path = require("path");
 const { SourceMapDevToolPlugin } = require("webpack");
 
 module.exports = {
-    entry: {
-        main: './src/index.js',
-        login: './src/app/login/login.js',
-        list: './src/app/components/listing/list.js',
-        create_account: './src/app/create_account/create_account.js'
+  entry: {
+    main: "./src/index.js",
+    login: "./src/app/login/login.js",
+    list: "./src/app/components/listing/list.js",
+    create_account: "./src/app/create_account/create_account.js",
+    home: "./src/app/components/home/home.js",
+    menu_listing: "./src/app/menu_listing/menu_listing.js",
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+    sourceMapFilename: "[name].js.map",
+  },
+  mode: "development",
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.js$/,
+        enforce: "pre",
+        use: ["source-map-loader"],
+      },
+    ],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "."),
     },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: "[name].js",
-        sourceMapFilename: "[name].js.map"
-    },
-    mode: 'development',
-    devtool: "source-map",
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader',
-                ],
-            },
-            {
-                test: /\.js$/,
-                enforce: "pre",
-                use: ["source-map-loader"],
-            },
-        ]
-    },
-    devServer: {
-        static: {
-            directory: path.join(__dirname, '.'),
-        },
-        port: 9000,
-    },
-    plugins: [
-        new SourceMapDevToolPlugin({
-            filename: "[file].map"
-        }),
-    ]
+    port: 9000,
+  },
+  plugins: [
+    new SourceMapDevToolPlugin({
+      filename: "[file].map",
+    }),
+  ],
 };
