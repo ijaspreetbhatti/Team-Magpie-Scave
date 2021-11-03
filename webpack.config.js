7033ae059ea75ac4ad37ab31a0ff50609d981ea1
olimarie21 <olivia.underdah@gmail.com>
2021-11-03 13:28:29 -0700

    add search.js to webpack file

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,7 @@ module.exports = {
         menu_account: './src/app/menu/account/menu_account.js',
         notification: './src/app/menu/notification/notification.js',
         map: './src/app/components/map/map.js',
+        search: './src/app/components/header/search.js',
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
