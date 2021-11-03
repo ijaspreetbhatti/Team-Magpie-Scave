0cf49a4dcfea9d77a3fe2ab9a0d2a97d607a506e
olimarie21 <olivia.underdah@gmail.com>
2021-11-03 14:01:40 -0700

    fix error in search input between mobile and desktop views

--- src/app/components/header/search.js
+++ src/app/components/header/search.js
@@ -26,6 +26,9 @@ search.addWidgets([
 
 function toggleSearch() {
 
+
+    const width = window.matchMedia("(min-width: 1000px)");
+
     console.log('click');
 
     const search = document.getElementById("headerSearch");
@@ -37,9 +40,7 @@ function toggleSearch() {
     const hamburger = document.getElementById("hamburger");
     const close = document.getElementById("closeSearch");
 
-    const width = screen.width; 
-
-    if (search.style.display === "inherit") {
+    if (!width.matches && search.style.display === "flex") {
         search.style.display = "none";
         addItem.style.display = "none";
         nav.style.display = "none";
@@ -50,11 +51,10 @@ function toggleSearch() {
         searchInput.style.display = "block";
         enter.style.display = "block";
     } else {
-
-        search.style.display = "inherit";
-        addItem.style.display = "inherit";
-        nav.style.display = "inherit";
-        logo.style.display = "inherit";
+        search.style.display = "flex";
+        addItem.style.display = "flex";
+        nav.style.display = "none";
+        logo.style.display = "flex";
         hamburger.style.display = "inherit";
 
         searchInput.style.display = "none";
@@ -67,6 +67,8 @@ window.toggleSearch = toggleSearch;
 
 function closeSearch() {
 
+    const width = window.matchMedia("(min-width: 1000px)");
+
     const search = document.getElementById("headerSearch");
     const addItem = document.getElementById("listItem");
     const nav = document.getElementById("navItems");
@@ -76,18 +78,28 @@ function closeSearch() {
     const hamburger = document.getElementById("hamburger");
     const close = document.getElementById("closeSearch");
 
-    if (search.style.display === "none") {
-        search.style.display = "inherit";
-        addItem.style.display = "inherit";
-        nav.style.display = "inherit";
-        logo.style.display = "inherit";
+    if (!width.matches && search.style.display === "none") {
+        search.style.display = "flex";
+        addItem.style.display = "flex";
+        nav.style.display = "none";
+        logo.style.display = "flex";
         hamburger.style.display = "inherit";
 
         searchInput.style.display = "none";
         enter.style.display = "none";
         close.style.display = "none";
+    } else if(width.matches) {
+        search.style.display = "none";
+        addItem.style.display = "flex";
+        nav.style.display = "block";
+        logo.style.display = "flex";
+        hamburger.style.display = "none";
+
+        searchInput.style.display = "flex";
+        enter.style.display = "flex";
+        close.style.display = "none";
     }
-    }
+}
 
 window.closeSearch = closeSearch;
 
@@ -111,3 +123,41 @@ function toggleNav() {
 };
 
 window.toggleNav = toggleNav;
+
+function headerFix(x) {
+
+    const search = document.getElementById("headerSearch");
+    const addItem = document.getElementById("listItem");
+    const nav = document.getElementById("navItems");
+    const logo = document.getElementById("mainLogo");
+    const searchInput = document.getElementById("searchInput");
+    const enter = document.getElementById("searchSubmit");
+    const hamburger = document.getElementById("hamburger");
+    const close = document.getElementById("closeSearch");
+
+    if (width.matches) { // If media query matches
+        search.style.display = "none";
+        addItem.style.display = "flex";
+        nav.style.display = "block";
+        logo.style.display = "flex";
+        hamburger.style.display = "none";
+
+        searchInput.style.display = "flex";
+        enter.style.display = "flex";
+        close.style.display = "none";
+    } else if (!width.matches && search.style.display === "none") {
+        search.style.display = "flex";
+        addItem.style.display = "flex";
+        nav.style.display = "none";
+        logo.style.display = "flex";
+        hamburger.style.display = "inherit";
+
+        searchInput.style.display = "none";
+        enter.style.display = "none";
+        close.style.display = "none";
+    }
+}
+
+const width = window.matchMedia("(min-width: 1000px)")
+headerFix(width) // Call listener function at run time
+width.addListener(headerFix) // Attach listener function on state changes
