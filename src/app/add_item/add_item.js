870209b03c8a798c07ec06bb29539d73c14abadd
olimarie21 <olivia.underdah@gmail.com>
2021-11-08 09:26:58 -0800

    Create func to hide initial photo upload prompt

--- src/app/add_item/add_item.js
+++ src/app/add_item/add_item.js
@@ -20,3 +20,29 @@ function photoUpload(event, upload, background, hide) {
 }
 
 window.photoUpload = photoUpload;
+
+// Func for initial first photo selection
+
+function firstUpload(event, upload, background, hide, hide2) {
+    console.log('img');
+    const img = event.target.files[0];
+    const reader = new FileReader();
+
+    let newImg = upload;
+    newImg.title = img.name;
+
+    reader.onload = function(event) {
+        newImg.src = event.target.result;
+    };
+
+    reader.readAsDataURL(img);
+
+    document.querySelectorAll(".photoInsert").forEach(a=>a.style.display = "flex");
+
+    upload.style.display = "block"
+    background.style.background = "white";
+    hide.style.display = "none";
+    hide2.style.display = "none";
+}
+
+window.firstUpload = firstUpload;
