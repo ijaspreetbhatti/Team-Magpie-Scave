2b0e9458c3d1ab10d2bddeec649e2861eb01e1df
olimarie21 <olivia.underdah@gmail.com>
2021-11-08 10:27:42 -0800

    Create img remove func for list item upload

--- src/app/add_item/add_item.js
+++ src/app/add_item/add_item.js
@@ -1,6 +1,6 @@
 // Select Images from File and Display on UI
 
-function photoUpload(event, upload, background, hide) {
+function photoUpload(event, upload, background, hide, show) {
     console.log('img');
     const img = event.target.files[0];
     const reader = new FileReader();
@@ -17,13 +17,14 @@ function photoUpload(event, upload, background, hide) {
     upload.style.display = "block"
     background.style.background = "white";
     hide.style.display = "none";
+    show.style.display = "block";
 }
 
 window.photoUpload = photoUpload;
 
 // Func for initial first photo selection
 
-function firstUpload(event, upload, background, hide, hide2) {
+function firstUpload(event, upload, background, hide, hide2, show) {
     console.log('img');
     const img = event.target.files[0];
     const reader = new FileReader();
@@ -43,6 +44,19 @@ function firstUpload(event, upload, background, hide, hide2) {
     background.style.background = "white";
     hide.style.display = "none";
     hide2.style.display = "none";
+    show.style.display = "block";
 }
 
 window.firstUpload = firstUpload;
+
+function removeImg(img, show, hide, hide2, background) {
+    img.src = "";
+    
+    show.style.display = "flex";
+    background.style.background = "#CFD8DC";
+    hide.style.display = "none";
+    hide2.style.display = "none";
+};
+
+window.removeImg = removeImg;
+
