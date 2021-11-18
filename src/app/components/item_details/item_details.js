375a8d5734145d3c08ea8012de1683921597a217
olimarie21 <olivia.underdah@gmail.com>
2021-11-18 12:02:45 -0800

    testing small map display

--- src/app/components/item_details/item_details.js
+++ src/app/components/item_details/item_details.js
@@ -75,3 +75,13 @@ window.onclick = function (e) {
         modal.style.display = 'none';
     }
 }
+
+function smallMap() {
+    map = new google.maps.Map(document.getElementById('smallMap'), {
+        disableDefaultUI: true,
+        zoom: 18,
+        center: {lat: currentItem.lat, lng: currentItem.lng},
+    });
+}
+
+window.smallMap = smallMap;
