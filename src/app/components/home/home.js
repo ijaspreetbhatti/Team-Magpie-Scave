f267b70b632e998909cbc78573cdb3cf5b80f882
Yebincho03 <lucycho0314@gmail.com>
2021-11-19 18:26:33 -0800

    Home and menu_listing update

--- src/app/components/home/home.js
+++
@@ -1,5 +0,0 @@
-import "./home.scss";
-import { database } from "../../services/firebase-service";
-document.getElementById("joinbtn").addEventListener("click", (e) => {
-  location.href = "/src/app/creat_accout/create_accout.html";
-});
--- src/index.js
+++ src/index.js
@@ -2,79 +2,81 @@ import "./app/components/header/search";
 import "./app/add_item/add_item";
 import "./main.scss";
 import * as $ from "jquery";
-import { app } from "./app/services/firebase-service"
-import { getAuth } from "firebase/auth"
+import { app } from "./app/services/firebase-service";
+import { getAuth } from "firebase/auth";
 
 let currentUser;
 window.currentUser = currentUser;
 const views = [
-    "addItemView",
-    // "mapView",
-    "listView",
-    "menuListingView",
-    "createAccountView",
-    "menuAccountView",
-    "notificationView",
-    "detailsView",
-    "searchView"
+  "addItemView",
+  // "mapView",
+  "listView",
+  "menuListingView",
+  "createAccountView",
+  "menuAccountView",
+  "notificationView",
+  "detailsView",
+  "searchView",
 ];
 
 // init method
 function init() {
-    console.log("initializing");
-    checkLoginState();
-    location.hash = "mapView";
-    switchView(location.hash);
+  console.log("initializing");
+  checkLoginState();
+  location.hash = "mapView";
+  switchView(location.hash);
 }
 
 init();
 
 window.addEventListener("hashchange", () => {
-    console.log(`Switching view to ${location.hash}`);
-    switchView(location.hash);
+  console.log(`Switching view to ${location.hash}`);
+  switchView(location.hash);
 });
 
 logOut.addEventListener("click", () => {
-    signOut();
+  signOut();
 });
 
 function checkLoginState() {
-    let auth = getAuth(app);
-    console.log(auth.currentUser);
-    let user = localStorage.getItem("user");
-    console.log(user);
-    if (user) {
-        currentUser = JSON.parse(user);
-        headerUserId.innerHTML = currentUser.displayName ? currentUser.displayName : 'Hello!';
-        headerMailId.innerHTML = currentUser.email;
-        console.log(currentUser);
-    } else {
-        location.replace("/login.html");
-    }
+  let auth = getAuth(app);
+  console.log(auth.currentUser);
+  let user = localStorage.getItem("user");
+  console.log(user);
+  if (user) {
+    currentUser = JSON.parse(user);
+    headerUserId.innerHTML = currentUser.displayName
+      ? currentUser.displayName
+      : "Hello!";
+    headerMailId.innerHTML = currentUser.email;
+    console.log(currentUser);
+  } else {
+    location.replace("/home.html");
+  }
 }
 
 function signOut() {
-    localStorage.removeItem("user");
-    location.replace("/");
+  localStorage.removeItem("user");
+  location.replace("/");
 }
 
 function switchView(id) {
-    if(id === "#mapView") {
-        $(".page-container").hide();
-    } else {
-        $(".page-container").show();
-    }
-    hideAllViews();
-    showView(id);
+  if (id === "#mapView") {
+    $(".page-container").hide();
+  } else {
+    $(".page-container").show();
+  }
+  hideAllViews();
+  showView(id);
 }
 
 function hideAllViews() {
-    views.forEach((id) => {
-        console.log(`#${id}`);
-        $(`#${id}`).hide();
-    });
+  views.forEach((id) => {
+    console.log(`#${id}`);
+    $(`#${id}`).hide();
+  });
 }
 
 function showView(id) {
-    $(`${id}`).fadeIn();
+  $(`${id}`).fadeIn();
 }
