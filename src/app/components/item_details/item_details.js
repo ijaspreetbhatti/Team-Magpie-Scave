fff6a747a3e3cb83d59cc0f8f738aac5d40bd92d
Gavriil84 <gabwt.84@gmail.com>
2021-11-21 20:03:38 -0800

    implement code for time count

--- src/app/components/item_details/item_details.js
+++ src/app/components/item_details/item_details.js
@@ -23,6 +23,30 @@ function populateListing() {
         .getElementById("mySlides")
         .addEventListener("click", mySlidesHandler);
 
+    // function timeSince(timeStamp) {
+    //     let now = new Date(),
+    //         secondsPast = (now.getTime() - timeStamp) / 1000;
+    //     if (secondsPast < 60) {
+    //         return parseInt(secondsPast) + 's';
+    //     }
+    //     if (secondsPast < 3600) {
+    //         return parseInt(secondsPast / 60) + 'm';
+    //     }
+    //     if (secondsPast <= 86400) {
+    //         return parseInt(secondsPast / 3600) + 'h';
+    //     }
+    //     if (secondsPast > 86400) {
+    //         day = timeStamp.getDate();
+    //         month = timeStamp.toDateString().match(/ [a-zA-Z]*/)[0].replace(" ", "");
+    //         year = timeStamp.getFullYear() == now.getFullYear() ? "" : " " + timeStamp.getFullYear();
+    //         return day + " " + month + year;
+    //     }
+    // }
+
+    // const postedTime = currentItem.date;
+    // const posted = timeSince(postedTime);
+    // console.log(posted)
+
     smallMap();
 }
 
