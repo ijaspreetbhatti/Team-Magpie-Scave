663340c5815bfd894a483815d545337e80bca52f
Mizzoo <87053514+Mizzuu@users.noreply.github.com>
2021-11-22 18:30:35 -0800

    get login userinfo and fix popup UI

--- src/app/components/menu/account/menu_account.js
+++ src/app/components/menu/account/menu_account.js
@@ -1,53 +1,51 @@
 import { app } from "../../../services/firebase-service";
-// import { getAuth } from "firebase/auth";
-
-// const auth = getAuth(app);
-// const user = auth.currentUser;
-// console.log(user);
-
-
-
 import { getAuth } from "firebase/auth";
 
-const auth = getAuth(app);
-const user = auth.currentUser;
-if (user !== null) {
-  // The user object has basic properties such as display name, email, etc.
-  const displayName = user.displayName;
-  const email = user.email;
-  const emailVerified = user.emailVerified;
-
-  console.log(displayName);
-  console.log("otter");
-
-  // The user's ID, unique to the Firebase project. Do NOT use
-  // this value to authenticate with your backend server, if
-  // you have one. Use User.getToken() instead.
-  const uid = user.uid;
+/* Get Logged In User's Information **************************/
+function checkLoginState() {
+    let auth = getAuth(app);
+    console.log(auth.currentUser);
+    let user = localStorage.getItem("user");
+    console.log(user);
+    if (user) {
+    //   let fullname = currentUser.displayName;
+    // let surname = fullname.split(" ").getOrNull(1);
+    // console.log(surname);
+        currentUser = JSON.parse(user);
+        acc_fName.value = currentUser.displayName
+            ? currentUser.displayName
+            : "John";
+        acc_lName.value = currentUser.displayName
+            ? currentUser.displayName
+            : "Doe";
+        acc_email.value = currentUser.email;
+        console.log(currentUser);
+    } else {
+        location.replace("/login.html");
+    }
+    
 }
 
-
-
-
+checkLoginState();
 
 /* Leave Confirmation *****************************/
 function popupModal() {
-  let popup = document.getElementById('js-popup');
-  if(!popup) return;
+    let popup = document.getElementById("js-popup");
+    if (!popup) return;
 
-  let blackBg = document.getElementById('js-black-bg');
-  let stayBtn = document.getElementById('js-stay-btn');
-  let showBtn = document.getElementById('mainLogo');
+    let blackBg = document.getElementById("js-black-bg");
+    let stayBtn = document.getElementById("js-stay-btn");
+    let showBtn = document.getElementById("mainLogo");
 
-  closePopUp(blackBg);
-  closePopUp(stayBtn);
-  closePopUp(showBtn);
-  function closePopUp(elem) {
-    if(!elem) return;
-    elem.addEventListener('click', function() {
-      console.log("gomi");
-      popup.classList.toggle('is-show');
-    });
-  }
+    closePopUp(blackBg);
+    closePopUp(stayBtn);
+    closePopUp(showBtn);
+    function closePopUp(elem) {
+        if (!elem) return;
+        elem.addEventListener("click", function () {
+            console.log("gomi");
+            popup.classList.toggle("is-show");
+        });
+    }
 }
 popupModal();
