9fdd3bc14cf50f301137febf5b07c77860a01b1e
Gavriil84 <gabwt.84@gmail.com>
2021-11-22 18:24:06 -0800

    clean code

--- src/app/components/item_details/item_details.js
+++ src/app/components/item_details/item_details.js
@@ -23,54 +23,6 @@ function populateListing() {
         .getElementById("mySlides")
         .addEventListener("click", mySlidesHandler);
 
-
-    function timeDifference(current, previous) {
-
-        var msPerMinute = 60 * 1000;
-        var msPerHour = msPerMinute * 60;
-        var msPerDay = msPerHour * 24;
-        var msPerMonth = msPerDay * 30;
-        var msPerYear = msPerDay * 365;
-
-        var elapsed = current - previous;
-
-        if (elapsed < msPerMinute) {
-            return Math.round(elapsed / 1000) + ' seconds ago';
-        }
-
-        else if (elapsed < msPerHour) {
-            return Math.round(elapsed / msPerMinute) + ' minutes ago';
-        }
-
-        else if (elapsed < msPerDay) {
-            return Math.round(elapsed / msPerHour) + ' hours ago';
-        }
-
-        else if (elapsed < msPerMonth) {
-            return 'approximately ' + Math.round(elapsed / msPerDay) + ' days ago';
-        }
-
-        else if (elapsed < msPerYear) {
-            return 'approximately ' + Math.round(elapsed / msPerMonth) + ' months ago';
-        }
-
-        else {
-            return 'approximately ' + Math.round(elapsed / msPerYear) + ' years ago';
-        }
-    }
-
-    const d = new Date().toJSON();
-    // let time = d.getTime();
-    const previous = currentItem.date
-    const postedTime = timeDifference(d, previous);
-    currentItem.posted = postedTime
-
-    const diff = d - previous
-
-    console.log(previous);
-    console.log(d);
-    console.log(diff);
-
     smallMap();
 }
 
