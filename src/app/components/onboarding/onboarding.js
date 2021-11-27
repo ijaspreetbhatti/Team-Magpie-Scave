4d07ecc7498540ef1ddee697982398b2d0583a3c
Yebincho03 <lucycho0314@gmail.com>
2021-11-27 14:00:37 -0800

    update

--- src/app/components/onboarding/onboarding.js
+++ src/app/components/onboarding/onboarding.js
@@ -1,1 +1,39 @@
 import "./onboarding.scss";
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+  console.log("working");
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+  console.log("working");
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+
+document.querySelector(".prev").addEventListener(plusSlides(-1));
+document.querySelector(".next").addEventListener(currentSlide(1));
+document.querySelector(".dot1").addEventListener(plusSlides(-1));
+document.querySelector(".dot").addEventListener(currentSlide(1));
--- src/app/login/login.js
+++ src/app/login/login.js
@@ -1,42 +1,41 @@
-import './login.scss'
+import "./login.scss";
 import { app } from "../services/firebase-service";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
 const auth = getAuth(app);
 function loginAccount(email, password) {
-    signInWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-            console.log(userCredential);
-            const user = userCredential.user;
-            localStorage.setItem('user', JSON.stringify(user));
-            location.replace('/');
-        })
-        .catch((error) => {
-            console.log(error);
-            const errorCode = error.code;
-            const errorMessage = error.message;
-        });
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      console.log(userCredential);
+      const user = userCredential.user;
+      localStorage.setItem("user", JSON.stringify(user));
+      location.replace("/");
+    })
+    .catch((error) => {
+      console.log(error);
+      const errorCode = error.code;
+      const errorMessage = error.message;
+    });
 }
 
 const loginForm = document.getElementById("login_form");
 const button = document.getElementById("btn");
 
-
-button.addEventListener('click', (e) => {
-    e.preventDefault();
-    console.log("clicked");
-    const email = document.getElementById('email').value;
-    const pass = document.getElementById('pass').value;
-    loginAccount(email, pass);
-})
+button.addEventListener("click", (e) => {
+  e.preventDefault();
+  console.log("clicked");
+  const email = document.getElementById("email").value;
+  const pass = document.getElementById("pass").value;
+  loginAccount(email, pass);
+});
 
 let currentUser;
 
 function checkLoginState() {
-    currentUser = localStorage.getItem('user');
-    if(currentUser) {
-        location.replace('/');
-    }
+  currentUser = localStorage.getItem("user");
+  if (currentUser) {
+    location.replace("/");
+  }
 }
 
 checkLoginState();
